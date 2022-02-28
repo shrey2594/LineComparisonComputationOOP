50162cc81d7901c6fb46ae222bf8f9e650c0efa7
shrey2594 <shrey2510@gmail.com>
2022-02-28 16:42:36 +0530

    Welcome to Line Comparison Problem

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace LineComparisonProblemOOP
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Problem using OOP Concept.");
+            Console.WriteLine("");
+            Console.WriteLine("Enter x coordinate of point1");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y coordinate of point1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x coordinate of point2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y coordinate of point2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            LengthCalculation lengthCalculation = new LengthCalculation();
+            lengthCalculation.calculateLength(x1,y1,x2,y2);
         }
     }
 
