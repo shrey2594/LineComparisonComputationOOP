e28f6b6ef48db79f40c2dd6eea82d9bebf9afa37
shrey2594 <shrey2510@gmail.com>
2022-02-28 17:31:19 +0530

    Returns larger line

---
+++ LengthComparison.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblemOOP
+{
+    public class LengthComparison
+    {
+        public void Comparison(double Length1, double Length2)
+        {
+            if (Length1 < Length2)
+            {
+                Console.WriteLine("Line1 is shorter than Line2");
+            }
+            else
+            {
+                Console.WriteLine("Line2 is shorter than Line1");
+            }
+        }
+    }
+}
